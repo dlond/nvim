8ca9c30a44069320b41cb181ad414682c016f644
dlond <dlond@me.com>
2025-10-21 19:12:34 +1300

    removed extra args

--- lua/plugins/spec/formatting.lua
+++ lua/plugins/spec/formatting.lua
@@ -29,12 +29,6 @@ return {
         yaml = { 'prettier' },
         markdown = { 'prettier' },
       },
-      -- Custom formatter configurations
-      formatters = {
-        ocamlformat = {
-          prepend_args = { '--enable-outside-detected-project' },
-        },
-      },
       -- Configure format_on_save behavior
       format_on_save = {
         timeout_ms = 2000, -- Increased for ocamlformat (can be slow)

8008d0c55722bd0d1956fb7ad852dcad3d019366
dlond <dlond@me.com>
2025-10-21 15:54:04 +1300

    plugins released

--- lua/plugins/spec/init.lua
+++ lua/plugins/spec/init.lua
@@ -5,21 +5,26 @@ return {
   { import = 'plugins.spec.editor' },
   { import = 'plugins.spec.autopairs' },
   { import = 'plugins.spec.indent-line' },
-  
+
   -- Core functionality
   { import = 'plugins.spec.lsp' },
   { import = 'plugins.spec.treesitter' },
   { import = 'plugins.spec.telescope' },
   { import = 'plugins.spec.blink' },
-  
+
   -- Git integration
   { import = 'plugins.spec.git' },
-  
+
   -- Development tools
+  { import = 'plugins.spec.ocaml' },
+  { import = 'plugins.spec.jupyter' },
   { import = 'plugins.spec.copilot' },
   { import = 'plugins.spec.debug' },
   { import = 'plugins.spec.formatting' },
-  
+  { import = 'plugins.spec.leetcode' },
+
   -- Navigation
   { import = 'plugins.spec.nvim-tmux-navigator' },
+  { import = 'plugins.spec.harpoon' },
 }
+

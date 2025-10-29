e61e0f58bc564883e1ff3cede4f6ba72574648c1
dlond <dlond@me.com>
2025-10-30 00:02:20 +1300

    tweak keymaps

--- lua/plugins/config/molten.lua
+++ lua/plugins/config/molten.lua
@@ -4,15 +4,15 @@ function M.setup()
   -- Keybindings
   local map = vim.keymap.set
 
-  map('n', '<leader>mi', ':MoltenInit<CR>', { desc = 'Molten: Initialize' })
-  map('n', '<leader>me', ':MoltenEvaluateOperator<CR>', { desc = 'Molten: Evaluate operator' })
-  map('v', '<leader>me', ':<C-u>MoltenEvaluateVisual<CR>gv', { desc = 'Molten: Evaluate visual' })
-  map('n', '<leader>ml', ':MoltenEvaluateLine<CR>', { desc = 'Molten: Evaluate line' })
-  map('n', '<leader>mr', ':MoltenReevaluateCell<CR>', { desc = 'Molten: Re-evaluate cell' })
-  map('n', '<leader>md', ':MoltenDelete<CR>', { desc = 'Molten: Delete cell' })
-  map('n', '<leader>mo', ':MoltenShowOutput<CR>', { desc = 'Molten: Show output' })
-  map('n', '<leader>mh', ':MoltenHideOutput<CR>', { desc = 'Molten: Hide output' })
-  map('n', '<leader>mx', ':MoltenInterrupt<CR>', { desc = 'Molten: Interrupt kernel' })
+  map('n', '<leader>Mi', ':MoltenInit<CR>', { desc = 'Molten: Initialize' })
+  map('n', '<leader>Me', ':MoltenEvaluateOperator<CR>', { desc = 'Molten: Evaluate operator' })
+  map('v', '<leader>Me', ':<C-u>MoltenEvaluateVisual<CR>gv', { desc = 'Molten: Evaluate visual' })
+  map('n', '<leader>Ml', ':MoltenEvaluateLine<CR>', { desc = 'Molten: Evaluate line' })
+  map('n', '<leader>Mr', ':MoltenReevaluateCell<CR>', { desc = 'Molten: Re-evaluate cell' })
+  map('n', '<leader>Md', ':MoltenDelete<CR>', { desc = 'Molten: Delete cell' })
+  map('n', '<leader>Mo', ':MoltenShowOutput<CR>', { desc = 'Molten: Show output' })
+  map('n', '<leader>Mh', ':MoltenHideOutput<CR>', { desc = 'Molten: Hide output' })
+  map('n', '<leader>Mx', ':MoltenInterrupt<CR>', { desc = 'Molten: Interrupt kernel' })
 end
 
 return M

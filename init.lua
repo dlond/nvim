7054437f2832aba8d32cbbad79d7925dcadb3bb7
dlond <dlond@me.com>
2026-04-12 10:57:42 +1200

    rearrange config

--- init.lua
+++ init.lua
@@ -4,9 +4,9 @@ require 'core.bootstrap'
 require 'core.global'
 -- Core settings (must be before plugins)
 require 'core.options'
+require 'core.lsp'
 require 'core.keymaps'
 require 'core.autocmds'
-require 'core.lsp'
 
 vim.env.DEVELOPER_DIR = '/Applications/Xcode.app/Contents/Developer'
 
